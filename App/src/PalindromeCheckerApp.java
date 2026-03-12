abcae9abc2434f63f3ec1b102825432c78121233
Dishan-web <dd9106@srmist.edu.in>
2026-03-12 15:20:54 +0530

    [Dishan DP] Added HardcodedPalindromeResult

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
-public class PalindromeCheckerApp {
-       public static void main(String[] args) {
-           System.out.println("Welcome To Palindrome Checker App System");
-       }
+public class PalindroneCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Hard Code Palindrome ");
+    }
 }
